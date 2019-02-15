04378684d9116837db976d7a43816b2efd456b88
ShaiMahmoud <38403237+haneensh@users.noreply.github.com>
2019-02-15 07:59:35 +0300

    modern

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {  
+    $("html").niceScroll({
+        cursorcolor: "#dbb65e",
+        cursorwidth: "5px",
+        cursorborder: "1px solid #6c6b66", 
+        cursorborderradius: "5px",
+        scrollspeed: 66
+    });
+    jQuery("h1 ").fitText();
+});
